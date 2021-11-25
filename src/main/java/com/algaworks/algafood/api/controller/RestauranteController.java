0b36bc7c4f454b8db9ac10a545d806d5dff9020e
Daniel Dutra <danieldhsd@gmail.com>
2021-11-24 22:09:59 -0300

    Implementando o cadastro e busca de restaurantes

---
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -0,0 +1,58 @@
+package com.algaworks.algafood.api.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.RestauranteService;
+
+@RestController
+@RequestMapping("/estados")
+public class RestauranteController {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private RestauranteService restauranteService;
+	
+	@PostMapping
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+		try {
+			restaurante = restauranteService.salvar(restaurante);
+			
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@GetMapping
+	public List<Restaurante> listar() {
+		return restauranteRepository.findAll();
+	}
+	
+	@GetMapping("/{restauranteId}")
+	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
+		Optional<Restaurante> restauranteOptional = restauranteRepository.findById(restauranteId);
+		
+		if(restauranteOptional.isPresent()) {
+			return ResponseEntity.ok(restauranteOptional.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+}
---
+++ src/main/java/com/algaworks/algafood/domain/service/RestauranteService.java
@@ -0,0 +1,52 @@
+package com.algaworks.algafood.domain.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@Service
+public class RestauranteService {
+
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+		
+		if(!cozinha.isPresent()) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+		}
+		
+		restaurante.setCozinha(cozinha.get());
+		return restauranteRepository.save(restaurante);
+	}
+	
+	public void excluir(Long restauranteId) {
+		try {
+			restauranteRepository.deleteById(restauranteId);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe um cadastro de restaurante com código %d", restauranteId));
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format("Restaurante de código %d não pode ser removido, pois está em uso", restauranteId));
+		}
+	}
+}
